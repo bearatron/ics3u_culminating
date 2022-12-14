4c02e02550e50cf322c0c60c1a9259fcd370b924
bearatron <84403830+bearatron@users.noreply.github.com>
2022-12-13 19:03:55 -0500

    Initial main program commit

---
+++ Checkers.java
@@ -0,0 +1,22 @@
+// Names: Ethan Diep and Glen Lin
+// Date: December 13, 2022
+// Teacher: Ms. Basaraba
+// Description: This program makes a checkers game in the Console window
+
+import hsa.*;
+import java.awt.*;
+public class Checkers {
+    Console c;
+    public static void main(String[] args) {
+        Checkers g = new Checkers();
+        g.test();
+    }
+
+    public void test() {
+        c.println("Hello world");
+    }
+
+    public Checkers() {
+        c = new Console();
+    }
+}
