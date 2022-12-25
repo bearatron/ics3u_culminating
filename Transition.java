c76f1ad63f89daad53b0c48113e5fcfa4a2a9c2d
Glen Lin <glenlin7813@gmail.com>
2022-12-24 21:34:21 -0500

    made transition and also main screen

--- Transition.java
+++ Transition.java
@@ -15,13 +15,13 @@ public class Transition extends Thread {
 
     public void run() {
         try {
-            Thread.sleep(5000);
+            Thread.sleep(2000);
         } catch (Exception e) {}
         int i = 0;
         while (i <= 260) {
             c.setColor(new Color(255,255,255,Math.min(i, 255)));
             c.fillRect(0, 0, 1024, 728);
-            i += 10;
+            i += 15;
             try {
                 Thread.sleep(1);
             } catch (Exception e) {}
@@ -30,7 +30,7 @@ public class Transition extends Thread {
         while (i <= 260) {
             c.setColor(new Color(182,215,168,Math.min(i, 255)));
             c.fillRect(0, 0, 1024, 728);
-            i += 10;
+            i += 15;
             try {
                 Thread.sleep(1);
             } catch (Exception e) {}
